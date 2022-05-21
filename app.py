b5a75a9fc83c835e1fa1b8f46f569fc4eeea948b
aMiki1998 <66155948+aMiki1998@users.noreply.github.com>
2022-05-21 10:47:46 -0500

    test

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from pydantic import BaseModel
 import pandas as pd
 from sklearn import tree
 
+app = FastAPI()
+#(title="API de diagnóstico de covid 19",
+#           description="test",
+#           version="1.0.0")
+
+@app.get('/')
+async def index():
+    return 'test'
+""""
 class diagnostico(BaseModel):
     tos: bool
     cefalea: bool
@@ -53,3 +62,4 @@ async def diagnostico(diagnostico:diagnostico):
         diagnostico.dolor_pecho,
         diagnostico.otros_sintomas]])
     return str(res[0])
+""""
