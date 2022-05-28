7375e7691f6d049b310c2dd63e8a58d5a7087028
aMiki1998 <66155948+aMiki1998@users.noreply.github.com>
2022-05-27 22:59:27 -0500

    test

--- main.py
+++ main.py
@@ -1,65 +1,7 @@
-from xmlrpc.client import boolean
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-import pandas as pd
-from sklearn import tree
 
 app = FastAPI()
-#(title="API de diagnóstico de covid 19",
-#           description="test",
-#           version="1.0.0")
 
-@app.get('/')
-async def index():
-    return 'test'
-""""
-class diagnostico(BaseModel):
-    tos: bool
-    cefalea: bool
-    congestion_nasal: bool						
-    dificultad_respiratoria: bool
-    dolor_garganta: bool
-    fiebre: bool
-    diarrea: bool
-    nauseas: bool
-    anosmia_hiposmia: bool
-    dolor_abdominal: bool
-    dolor_articulaciones: bool
-    dolor_muscular: bool
-    dolor_pecho: bool
-    otros_sintomas: bool
-
-app = FastAPI(title="API de diagnóstico de covid 19",
-            description="test",
-            version="1.0.0")
-
-data = pd.read_excel (r'C:\Users\Miky\Documents\Personales\Maestría\2022-1\Programación Avanzada\Proyecto\Datasets\Dataset 2\Datasetpequeño_Listo.xlsx') 
-df = pd.DataFrame(data, columns= ['tos','cefalea','congestion_nasal','dificultad_respiratoria','dolor_garganta','fiebre','diarrea','nauseas','anosmia_hiposmia','dolor_abdominal','dolor_articulaciones','dolor_muscular','dolor_pecho','otros_sintomas'])
-labels = pd.DataFrame(data, columns= ['Flag_sospechoso'])
-classifier = tree.DecisionTreeClassifier()
-classifier.fit(df,labels)
-
-@app.get('/')
-async def index():
-    return 'test'
-
-@app.post('/Diagnostico')
-async def diagnostico(diagnostico:diagnostico):
-    res = classifier.predict([[
-        diagnostico.tos,
-        diagnostico.cefalea,
-        diagnostico.congestion_nasal,
-        diagnostico.dificultad_respiratoria,
-        diagnostico.dolor_garganta,
-        diagnostico.fiebre,
-        diagnostico.diarrea,
-        diagnostico.nauseas,
-        diagnostico.anosmia_hiposmia,
-        diagnostico.dolor_abdominal,
-        diagnostico.dolor_articulaciones,
-        diagnostico.dolor_muscular,
-        diagnostico.dolor_pecho,
-        diagnostico.otros_sintomas]])
-    return str(res[0])
-"""
+@app.get("/inicio")
+async def ruta_prueba():
+    return "Hola mundo"
