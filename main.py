9afd441a63f4a25312f28a67a5aa9d9a70f2c29b
aMiki1998 <66155948+aMiki1998@users.noreply.github.com>
2022-05-27 23:14:41 -0500

    test

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="API de diagnóstico de covid 19",
+           description="test",
+           version="1.0.0")
 
 @app.get("/inicio")
 async def ruta_prueba():
     return "Hola mundo"
+
+    
