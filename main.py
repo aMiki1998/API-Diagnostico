ce082537bc6682f473c4ae3635395dc8e4ecf328
aMiki1998 <66155948+aMiki1998@users.noreply.github.com>
2022-07-15 22:55:07 -0500

    new
    new

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="API de diagnóstico de covid 19",
             description="test",
             version="1.0.0")
 
-data = pd.read_excel ('Dataset_500000.xlsx')
+data = pd.read_excel ('Dataset_250000.xlsx')
 df = pd.DataFrame(data, columns= ['tos','cefalea','congestion_nasal','dificultad_respiratoria','dolor_garganta','fiebre','diarrea','nauseas','anosmia_hiposmia','dolor_abdominal','dolor_articulaciones','dolor_muscular','dolor_pecho','otros_sintomas'])
 labels = pd.DataFrame(data, columns= ['Flag_sospechoso'])
 classifier = tree.DecisionTreeClassifier()
