f4a946590d935c10306d20a55b40b1776e1808e3
aMiki1998 <66155948+aMiki1998@users.noreply.github.com>
2022-07-16 15:31:42 -0500

    New
    new

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 import pandas as pd
-from sklearn import tree
+
+from sklearn import tree 
+from sklearn.model_selection import train_test_split
+
 
 class diagnostico(BaseModel):
     tos: bool
@@ -25,11 +28,17 @@ app = FastAPI(title="API de diagnóstico de covid 19",
             description="test",
             version="1.0.0")
 
-data = pd.read_excel ('Dataset_250000.xlsx')
-df = pd.DataFrame(data, columns= ['tos','cefalea','congestion_nasal','dificultad_respiratoria','dolor_garganta','fiebre','diarrea','nauseas','anosmia_hiposmia','dolor_abdominal','dolor_articulaciones','dolor_muscular','dolor_pecho','otros_sintomas'])
-labels = pd.DataFrame(data, columns= ['Flag_sospechoso'])
-classifier = tree.DecisionTreeClassifier()
-classifier.fit(df,labels)
+data = pd.read_excel ('Dataset_No_Dupliclates_200000.xlsx')
+
+X = pd.DataFrame(data, columns= ['tos','cefalea','congestion_nasal','dificultad_respiratoria','dolor_garganta','fiebre','diarrea','nauseas','anosmia_hiposmia','dolor_abdominal','dolor_articulaciones','dolor_muscular','dolor_pecho','otros_sintomas'])
+y = pd.DataFrame(data, columns= ['Flag_sospechoso'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+classifier = tree.DecisionTreeClassifier(max_depth = 15,
+                                         class_weight={0:2.77})
+
+classifier.fit(X_train, y_train)
 
 @app.get('/')
 async def index():
